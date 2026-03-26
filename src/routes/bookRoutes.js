913ebc10e405b02fd78c57d154109f7cfa6ad554
Abraham <abnationtech@gmail.com>
2026-03-26 02:43:00 -1000

    fix3

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cloudinary from "../lib/cloudinary.js";
 import Book from "../models/Book.js";
-import protectRoute from "../middleware/auth.middleware.js";
+// import protectRoute from "../middleware/auth.middleware.js";
+import { protectRoute, adminOnly } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
